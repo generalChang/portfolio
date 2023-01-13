bbb464971bc639d8e5ca1d9d8d2604995d2913b4
generalChang <rhdrnsckdgh0921@naver.com>
2023-01-13 17:25:15 +0900

    javascript scroll to section

--- main.js
+++ main.js
@@ -10,3 +10,34 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+//scroll to section
+// const menus = document.querySelectorAll(".navbar__menu__item");
+// menus.forEach((menu) => {
+//   menu.addEventListener("click", () => {
+//     menus.forEach((m) => {
+//       m.classList.remove("active");
+//     });
+//     const dataId = menu.dataset.id;
+//     const targetElement = document.querySelector(`#${dataId}`);
+//     const location = targetElement.getBoundingClientRect().top;
+//     window.scrollBy({
+//       top: location,
+//       behavior: "smooth",
+//     });
+//     menu.classList.add("active");
+//   });
+// });
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (e) => {
+  const target = event.target;
+  const link = target.dataset.id;
+  if (link == null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(`#${link}`);
+  scrollTo.scrollIntoView({
+    behavior: "smooth",
+  });
+});

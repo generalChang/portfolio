943391a9bf494d1d0b5a4f9df2e6c68a475b7126
generalChang <rhdrnsckdgh0921@naver.com>
2023-01-14 17:18:00 +0900

    javascript work animation

--- main.js
+++ main.js
@@ -30,14 +30,88 @@ window.addEventListener("scroll", () => {
 // });
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (e) => {
-  const target = event.target;
+  const target = e.target;
   const link = target.dataset.id;
   if (link == null) {
     return;
   }
 
-  const scrollTo = document.querySelector(`#${link}`);
+  scrollIntoView(`#${link}`);
+});
+
+//handle click on contact me button on home
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  scrollIntoView(`#contact`);
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({
     behavior: "smooth",
   });
+}
+
+const home = document.querySelector(".home_container");
+const homeHeight = home.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+  home.style.setProperty("opacity", `${1 - scrollY / homeHeight}`);
 });
+
+const arrowBtn = document.querySelector(".arrow__button");
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+  if (scrollY > 400) {
+    arrowBtn.classList.add("active");
+  } else {
+    arrowBtn.classList.remove("active");
+  }
+});
+
+arrowBtn.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
+
+const categorys = document.querySelector(".work__categorys");
+const projectContainer = document.querySelector(".work__projects");
+categorys.addEventListener("click", (e) => {
+  const target = e.target;
+  const id = target.dataset.id || target.parentNode.dataset.id;
+  if (!id) {
+    return;
+  }
+
+  for (let a = 0; a < categorys.children.length; a++) {
+    const categoryBtn = categorys.children[a];
+    const targetId = categoryBtn.dataset.id;
+    if (id == targetId) {
+      categoryBtn.classList.add("active");
+    } else {
+      categoryBtn.classList.remove("active");
+    }
+  }
+
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => {
+    showWorksForCategory(id);
+    projectContainer.classList.remove("anim-out");
+  }, 300);
+});
+
+showWorksForCategory("all");
+function showWorksForCategory(category) {
+  const projects = document.querySelectorAll(".project");
+
+  projects.forEach((project) => {
+    const targetCategory = project.dataset.id;
+    if (category === targetCategory) {
+      project.classList.add("visible");
+    } else {
+      project.classList.remove("visible");
+      if (category === "all") {
+        project.classList.add("visible");
+      }
+    }
+  });
+}
